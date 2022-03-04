e472a663e4e3d50834ab11aceee99b841bf7ad34
rosessix <35843808+rosessix@users.noreply.github.com>
2022-03-04 09:14:35 +0100

    tilføjet sej besked

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     document.addEventListener('keydown', handleKeyPress)    
-    $('.modal').modal();
+    $('#patchnotes').modal();
+
+    firstTimePlaying()
 })
 
 const dictonary = [
@@ -10346,7 +10348,7 @@ function copyResult() {
 
 function copyTextToClipboard(text) {
     var textArea = document.createElement("textarea");
-  
+
     //
     // *** This styling is an extra step which is likely not required. ***
     //
@@ -10362,43 +10364,43 @@ function copyTextToClipboard(text) {
     // box asking the user for permission for the web page to
     // copy to the clipboard.
     //
-  
+
     // Place in the top-left corner of screen regardless of scroll position.
     textArea.style.position = 'fixed';
     textArea.style.top = 0;
     textArea.style.left = 0;
-  
+
     // Ensure it has a small width and height. Setting to 1px / 1em
     // doesn't work as this gives a negative w/h on some browsers.
     textArea.style.width = '2em';
     textArea.style.height = '2em';
-  
+
     // We don't need padding, reducing the size if it does flash render.
     textArea.style.padding = 0;
-  
+
     // Clean up any borders.
     textArea.style.border = 'none';
     textArea.style.outline = 'none';
     textArea.style.boxShadow = 'none';
-  
+
     // Avoid flash of the white box if rendered for any reason.
     textArea.style.background = 'transparent';
-  
-  
+
+
     textArea.value = text;
-  
+
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
-  
+
     try {
-      var successful = document.execCommand('copy');
-      var msg = successful ? 'successful' : 'unsuccessful';
-      console.log('Copying text command was ' + msg);
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
     } catch (err) {
-      console.log('Oops, unable to copy');
+        console.log('Oops, unable to copy');
     }
-  
+
     document.body.removeChild(textArea);
 }
 
@@ -10410,3 +10412,18 @@ function Toast(msg, classes, time) {
     })
 }
 
+function firstTimePlaying() {
+    let firstTime = localStorage.getItem('firsttime') == undefined
+
+    if (firstTime == true) {
+        const elem = document.getElementById('howtoplay');
+        const instance = M.Modal.init(elem, {
+            dismissible: false,
+            onCloseEnd: function() {
+                localStorage.setItem('firsttime', false)
+            }
+        });
+        instance.open();
+        console.log('modalled')
+    }
+}
